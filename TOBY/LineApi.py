5f98e308059d45c1791b8f35bb25e3a91310a91a
MintIvy <33442205+MintIvy@users.noreply.github.com>
2017-11-09 13:45:40 -0600

    Update LineApi.py

--- TOBY/LineApi.py
+++ TOBY/LineApi.py
@@ -311,7 +311,7 @@ class LINE:
 
       prof = self.getProfile()
 
-      print("MikanBOT")
+      print("SugarPlum")
       print("mid -> " + prof.mid)
       print("name -> " + prof.displayName)
       print("authToken -> " + self.authToken)
